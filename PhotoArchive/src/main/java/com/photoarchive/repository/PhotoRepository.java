15bf98ccd1d2a65e03e1369ed91a909f8a416bf2
YarosLewLion <hyperactiveanimalspirit@gmail.com>
2025-04-04 20:52:30 +0200

    Delete PhotoArchive directory

--- PhotoArchive/src/main/java/com/photoarchive/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.photoarchive.repository;
-
-import com.photoarchive.entity.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
